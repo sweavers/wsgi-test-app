01f9d16fbb70de1ab58bbf948609caf65c765cc3
Sebastian Weavers <sebastian.weavers@landregistry.gsi.gov.uk>
2015-09-23 10:56:52 +0100

    Revision 3

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! revision 2'
+    return 'Hello World! revision 3'
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', port=9000, debug=False)                   
